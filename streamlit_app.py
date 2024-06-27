c7130eefd0614b8e7db50bacccbc592550c52b61
lucymc768 <loomgotlost@gmail.com>
2024-06-27 14:17:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+from hugchat import hugchat
+from hugchat.login import Login
 
+st.set_page_config(page_title="🤗💬 HugChat")
 st.title('🎈 App Name')
 
 st.write('Hello world!')
+
